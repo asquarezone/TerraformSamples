b944e49e91b1a62366b0a1ffcebdd755e7dfdb99
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-03-16 11:56:20 +0530

    Added changes

---
+++ activity1/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.100.0.0/16"
+}
