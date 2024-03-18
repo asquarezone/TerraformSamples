7b7cec142c2f8c6d3268ec7d58ac3a55751eb167
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-03-18 11:29:27 +0530

    Added public and private route table and also internet gateway

--- ntier/main.tf
+++ ntier/main.tf
@@ -1,3 +1,4 @@
+# vpc
 resource "aws_vpc" "network" {
   cidr_block = var.vpc_cidr
   tags = {
@@ -7,6 +8,7 @@ resource "aws_vpc" "network" {
   }
 }
 
+# subnet
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
   vpc_id     = aws_vpc.network.id
@@ -18,4 +20,40 @@ resource "aws_subnet" "subnets" {
   }
   availability_zone = var.availability_zone_values[count.index]
   depends_on        = [aws_vpc.network]
+}
+
+# create and attach internet gateway
+resource "aws_internet_gateway" "ntier_igw" {
+  vpc_id = aws_vpc.network.id
+  tags = {
+    Name        = "ntier-igw"
+    Environment = "dev"
+    CreatedBy   = "terraform"
+  }
+  depends_on = [aws_vpc.network]
+}
+
+# create a public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.network.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ntier_igw.id
+  }
+  tags = {
+    Name        = "ntier-public-rt"
+    Environment = "dev"
+    CreatedBy   = "terraform"
+  }
+  depends_on = [aws_vpc.network, aws_subnet.subnets]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id     = aws_vpc.network.id
+  depends_on = [aws_vpc.network, aws_subnet.subnets]
+  tags = {
+    Name        = "ntier-private-rt"
+    Environment = "dev"
+    CreatedBy   = "terraform"
+  }
 }
