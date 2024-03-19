9e6bea1fa5f9bfa4dabe6812aea8ba3409ac49d1
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-03-19 12:09:31 +0530

    Added backend

---
+++ sampleforlearning/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_subnet" "test" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_subnet" "test1" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "test"
+  }
+}
---
+++ sampleforlearning/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dmttfstate"
+    key    = "sampleforlearning/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "dmttfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
